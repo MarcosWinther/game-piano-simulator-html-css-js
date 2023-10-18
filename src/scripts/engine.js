a99638290b46850530e74621e7911e761256b970
MarcosWinther <winthermarcos@gmail.com>
2023-10-18 10:46:42 -0300

    Mapeado as teclas permitidas e adicionado classes dinamicamente quando clicar nas teclas do piano

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,15 +1,29 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
 
+let mapedKeys = [];
+
 let audio = new Audio("src/tunes/1.wav");
 
 const playTune = (key) => {
    audio.src = `src/tunes/${key}.wav`;
    audio.play();
+
+   const clickedKey = document.querySelector(`[data-key="${key}"]`);
+   clickedKey.classList.add('active');
+   setTimeout(() => {
+      clickedKey.classList.remove('active')
+   }, 150);
 }
 
 pianoKeys.forEach((key) => {
-   console.log(key.dataset.key);
    key.addEventListener('click', () => playTune(key.dataset.key));
+   mapedKeys.push(key.dataset.key);
 });
 
-document.addEventListener('keydown', (e) => playTune(e.key));
+document.addEventListener('keydown', (e) => {
+
+   if(mapedKeys.includes(e.key)) {
+      playTune(e.key);
+   }
+
+});
